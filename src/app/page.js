2b389a4e6cb9b9487aaf19350636a0aa938635f6
sonalishetty04 <sonali@appinessworld.com>
2024-07-18 11:30:30 +0530

    advantages web version

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import AdvantageSection from "@/components/AdvantageSection";
 import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
     <>
       <HeroSection />
+      <AdvantageSection />
     </>
   );
 }
---
+++ src/components/AdvantageSection.jsx
@@ -0,0 +1,116 @@
+import Image from "next/image";
+import React from "react";
+
+function AdvantageSection() {
+  return (
+    <div className="w-full md:px-20 px-8">
+      <div className="w-full ">
+        <h1 className="text-5xl font-semibold text-center">
+          The SportStr <span className="text-green-500">Advantage</span>
+        </h1>
+
+        <div className="w-full mt-20">
+          {/* For Academy Owners and Administrators section */}
+
+          <div className="flex items-center w-full gap-10">
+            <div className="w-full  md:w-1/2 flex flex-col gap-5  ">
+              <h2 className="font-semibold text-xl">
+                FOR ACADEMY OWNERS & <br />
+                ADMINISTRATORS
+              </h2>
+
+              {/* Images for the mobile view */}
+
+              <div className="flex items-center  md:hidden">
+                <Image
+                  src="/nav-bar-items.webp"
+                  alt="nav-bar-items"
+                  width={133}
+                  height={395}
+                />
+                <Image
+                  src="/info-boards.webp"
+                  alt="info-boards"
+                  width={457}
+                  height={334}
+                />
+              </div>
+              <p className="text-sm text-gray-500 leading-6">
+                Streamline every aspect of academy management from scheduling to
+                attendance management to communication to billing and payment
+                collections. With automated notifications and a centralized
+                dashboard, reduce administrative tasks and focus more on
+                strategic growth. Experience efficient management and keep your
+                operations smooth and organized.
+              </p>
+            </div>
+            <div className=" hidden md:flex items-center  w-1/2">
+              <Image
+                src="/nav-bar-items.webp"
+                alt="nav-bar-items"
+                width={133}
+                height={395}
+              />
+              <Image
+                src="/info-boards.webp"
+                alt="info-boards"
+                width={457}
+                height={334}
+              />
+            </div>
+          </div>
+
+          {/* For coaches Section */}
+
+          <div className="flex items-center w-full gap-10 my-24">
+            <div className="w-1/2 hidden md:block">
+              <Image
+                src="/calendar.webp"
+                alt="calendar"
+                width={522}
+                height={524}
+              />
+            </div>
+            <div className=" w-1/2 flex flex-col gap-5">
+              <h2 className="font-semibold text-xl">FOR COACHES</h2>
+              <p className="text-sm text-gray-500 leading-6">
+                Streamline every aspect of academy management from scheduling to
+                attendance management to communication to billing and payment
+                collections. With automated notifications and a centralized
+                dashboard, reduce administrative tasks and focus more on
+                strategic growth. Experience efficient management and keep your
+                operations smooth and organized.
+              </p>
+            </div>
+          </div>
+
+          {/* For Athletes/Trainees Section */}
+
+          <div className="flex items-center w-full gap-10">
+            <div className=" w-1/2 flex flex-col gap-5">
+              <h2 className="font-semibold text-xl">FOR ATHLETES/TRAINEES</h2>
+              <p className="text-sm text-gray-500 leading-6">
+                Streamline every aspect of academy management from scheduling to
+                attendance management to communication to billing and payment
+                collections. With automated notifications and a centralized
+                dashboard, reduce administrative tasks and focus more on
+                strategic growth. Experience efficient management and keep your
+                operations smooth and organized.
+              </p>
+            </div>
+            <div className="w-1/2">
+              <Image
+                src="/calendar.webp"
+                alt="calendar"
+                width={522}
+                height={524}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default AdvantageSection;
