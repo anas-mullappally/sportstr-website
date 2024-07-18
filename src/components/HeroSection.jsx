ea558d37020720752fab063b4a68a909438cd6bd
sonalishetty04 <sonali@appinessworld.com>
2024-07-18 10:13:29 +0530

    added image sizes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,14 +2,20 @@ import Image from "next/image";
 
 function HeroSection() {
   return (
-    <div className="w-full flex px-8 py-14 md:px-20 md:py-32 items-center md:flex-row flex-col">
+    <div className="w-full flex px-8 py-14 md:px-20 md:py-32 items-center md:flex-row flex-col ">
       {/* Left content section */}
       <div className="flex flex-col gap-4 md:gap-8 lg:w-3/5 md:w-1/2 text-center md:text-left">
         <h1 className="text-2xl md:text-5xl font-bold w-full">
           Transforming <br /> Academy Management
         </h1>
         <div className="relative w-full max-w-[550px] h-auto">
-          <Image src="/curve.svg" alt="curve" width={550} height={540} />
+          <Image
+            src="/curve.svg"
+            alt="curve"
+            width={550}
+            height={540}
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
         </div>
         <p className="md:text-2xl text-base font-medium">
           Simplify, Manage and Enhance Your <br /> Coaching Operations
@@ -34,6 +40,7 @@ function HeroSection() {
             width={516}
             height={516}
             className="w-full h-full"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             priority
           />
         </div>
