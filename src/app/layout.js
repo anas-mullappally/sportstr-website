28043a7871f0c929145d8c644e9b6f7f70ab6fba
anas-mullappally <anas@appinessworld.com>
2024-07-23 17:36:43 +0530

    added speed insights of vercel

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Cabin } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const cabin = Cabin({ subsets: ["latin"] });
 
@@ -39,6 +40,7 @@ export default function RootLayout({ children }) {
         <Header />
         {children}
         <Footer />
+        <SpeedInsights />
       </body>
     </html>
   );
