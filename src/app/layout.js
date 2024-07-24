81bf78a19a61450f61d8ebf45a6d00f37fe4e9f6
anas-mullappally <anas@appinessworld.com>
2024-07-24 12:24:52 +0530

    added GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,10 +3,38 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-// import { GoogleTagManager } from "@next/third-parties/google";
+import { GoogleTagManager } from "@next/third-parties/google";
 import Script from "next/script";
 
 const cabin = Cabin({ subsets: ["latin"] });
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <GoogleTagManager gtmId="GTM-K5J68JFP" />
+      <head>
+        {/* icon  */}
+        <link rel="icon" href="/favicon.png" />
+      </head>
+      <body className={cabin.className}>
+        {/* Google Tag Manager (noscript)  */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-K5J68JFP"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+        <Header />
+        {children}
+        <Footer />
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
 
 export const metadata = {
   metadataBase: new URL(process.env.NEXT_PUBLIC_WEB_URL),
@@ -30,46 +58,3 @@ export const metadata = {
     },
   },
 };
-
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        {/* icon  */}
-        <link rel="icon" href="/favicon.png" />
-        {/* Google Tag Manager  */}
-        {/* <Script
-          id="gtm-script"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-K5J68JFP');
-  `,
-          }}
-        /> */}
-        {/* End Google Tag Manager */}
-      </head>
-      {/* <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GM_TAG} /> */}
-      <body className={cabin.className}>
-        {/* Google Tag Manager (noscript)  */}
-        {/* <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-K5J68JFP"
-            height="0"
-            width="0"
-            style="display:none;visibility:hidden"
-          ></iframe>
-        </noscript> */}
-        {/* End Google Tag Manager (noscript) */}
-        <Header />
-        {children}
-        <Footer />
-        <SpeedInsights />
-      </body>
-    </html>
-  );
-}
