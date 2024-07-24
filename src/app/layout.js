35ff4efe41e9caece4f386048f4b70d6de46ff10
anas-mullappally <anas@appinessworld.com>
2024-07-24 12:26:52 +0530

    added GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
             src="https://www.googletagmanager.com/ns.html?id=GTM-K5J68JFP"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{ display: "none", visibility: "hidden" }}
           ></iframe>
         </noscript>
         {/* End Google Tag Manager (noscript) */}
